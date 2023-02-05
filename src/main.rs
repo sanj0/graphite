95ada7d6608ec526e214aa71792a7fa108fe5e90
M <geotag-caddies8x@icloud.com>
2023-02-05 20:49:33 +0100

    feat(editing): implement dragging

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,31 @@ use edit_state::*;
 async fn main() {
     let mut graph = Graph::new();
     let mut edit = EditState::None(0);
+    // cursor on mouse down to distinguish between drag and click
+    let mut cursor0: Option<(f32, f32)> = None;
+    let mut mouse_dragging = false;
 
     loop {
         clear_background(WHITE);
-        if is_mouse_button_pressed(MouseButton::Left) {
-            edit = edit.left_mouse_pressed(mouse_position(), &mut graph);
+        if is_mouse_button_down(MouseButton::Left) {
+            if let Some(c) = cursor0 {
+                if c != mouse_position() {
+                    mouse_dragging = true;
+                }
+            } else {
+                cursor0 = Some(mouse_position());
+            }
+        } else if cursor0.is_some() {
+            if mouse_dragging {
+                mouse_dragging = false;
+                edit = edit.left_mouse_drag_stop();
+            } else {
+                edit = edit.left_mouse_clicked(mouse_position(), &mut graph);
+            }
+            cursor0 = None;
+        }
+        if mouse_dragging {
+            edit = edit.left_mouse_dragging(mouse_position(), &mut graph);
         }
         graph.render(edit.selected());
         next_frame().await;
